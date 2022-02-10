f8027aaf807bb5a678608bac3e4646c230b2e312
Mas-Akbar <96723103+Mas-Akbar@users.noreply.github.com>
2022-02-10 10:33:04 +0700

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,23 @@
+nilai = 6
+harga = input('berapa haraga barang?')
+if (nilai>7):
+    print('anda lulus')
+else:
+    print("maaf uangmu kurang, kamu harus pulang")
+
+#duit == 800
+
+if (harga == 100):
+    print (" uang sisa 900")
+elif (harga == 200):
+    print('uang sisa 800')
+elif (harga ==300):
+    print('uang sisa 700')
+
+tembok = 'kuning'
+if (tembok == 'hijau'):
+    print ('warna tembok sudah berubah')
+else:
+    print ( 'warna tembok mmasih seperti dulu, kuning')
+
+
